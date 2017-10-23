2ccc84c166e9f382f91ceacb7f5745f3cbb3ef76
Swaminathan <adswaminathan@lm-sjn-21001383.paypalcorp.com>
2017-10-23 16:18:42 -0700

    First commit to Web Server Application

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+
+// calling express as a function
+var app = express();
+
+// adding built-in middleware to tweak express
+// setting up a static directory for a web page
+app.use(express.static(__dirname+ '/public'));
+
+// register / set up a handler for a http get request
+// context route
+app.get('/', (req,res) => {
+  // req has the header, body, path of req
+  // res has bunch of methods to respond to req with status, response type etc
+
+  //res.send('<h1>Hello Express!</h1>'); // content type defaults to text/html
+  res.send({
+    name: 'Adi',
+    likes: [
+      'Coffee',
+      'Tennis'
+    ]
+  }); // applicatio/json
+});
+
+app.get('/about', (req,res) => {
+  res.send('about page');
+
+});
+
+app.get('/bad', (req,res) => {
+  res.send({
+    err: 'Unable to respond to request'
+  });
+
+});
+// binding the app to a port on our machine or a server machine
+app.listen(3000, ()=>{
+  console.log('Server is up on 3000 port');
+
+}); // can take a 2nd arg
